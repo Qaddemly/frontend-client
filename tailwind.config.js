629ad438c6c5e1c25cb00704db229b8bb56ccf30
Youssef_Gad <youssef.gad2003@yahoo.com>
2024-11-22 19:08:57 +0200

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    colors: {
+      main: "#133E87",
+      secondary: "#608BC1",
+      danger: "#D93A40",
+      "light-danger": "#FEEEEF",
+      "gray-100": "#ccc",
+      "gray-200": "#D9D9D9",
+      "gray-300": "#999",
+      "gray-400": "#9BA1AA",
+      "gray-500": "#767F8C",
+      "gray-600": "#5E6670",
+      "gray-700": "#4F4F4F",
+      "gray-800": "#2D2D2D",
+      "green-100": "#0BA02C",
+      "green-200": "#017550",
+      "light-green": "#E7F6EA",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
