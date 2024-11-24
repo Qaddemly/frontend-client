b6eaab60c1ffae8161e968eea56510c91c425851
ereny <nagyhosny919@gmail.com>
2024-11-24 23:09:19 +0200

    sec version

---
+++ src/components/home/Footer.tsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <div className="mt-20 bg-gray-800 px-6 pt-20 text-gray-100">
+      <span className="text-2xl font-bold">Qaddemly</span>
+    </div>
+  );
+}
+
+export default Footer;
--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -3,12 +3,14 @@ import Home from "../pages/Home";
 import Login from "../pages/Login";
 import Signup from "../pages/Signup";
 import EmailVerify from "../pages/EmailVerify";
+import Profile from "../pages/Profile";
 
 const router = createBrowserRouter([
   { path: "/", element: <Home /> },
   { path: "/login", element: <Login /> },
   { path: "/signup", element: <Signup /> },
   { path: "/emailVerfiy", element: <EmailVerify /> },
+  { path: "/profile", element: <Profile /> },
 ]);
 
 function AppRouter() {
