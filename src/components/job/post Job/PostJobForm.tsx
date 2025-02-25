559438bcc93e6e582b9d6cba8a575c26be7a75f3
AmAr <amar.glwsh28@gmail.com>
2025-02-25 04:07:19 +0200

    reversed

--- src/components/job/post Job/PostJobForm.tsx
+++ src/components/job/post Job/PostJobForm.tsx
@@ -112,7 +112,7 @@ function PostJobForm() {
       const res = await postNewJob(data as IPostNewJobInputs).unwrap();
       toast.success(res.message);
       refetch();
-      navigate(`/businessDashboard/companyJobs/${companyId}/active`);
+      navigate(`/businessDashboard/companyJobs/${companyId}`);
     } catch (error) {
       handleApiError(error);
     }
