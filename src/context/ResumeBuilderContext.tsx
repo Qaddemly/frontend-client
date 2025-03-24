61a4fb9c97d9375d4b71b9129dd6cbda2110bc03
Youssef_Gad <youssef.gad2003@yahoo.com>
2025-03-24 16:28:06 +0200

    fix some bugs with resume builder

--- src/context/ResumeBuilderContext.tsx
+++ src/context/ResumeBuilderContext.tsx
@@ -144,17 +144,17 @@ export const ResumeBuilderProvider: React.FC<{ children: ReactNode }> = ({
     resumeId && !status.includes("addResume") ? { resumeId } : skipToken,
   );
   ///////////////////////////////////////////// Languages //////////////////////////////////////////////
-  const { data: resumeLanguages } = useGetAllResumeLanguageQuery({
-    resumeId: resumeId || "",
-  });
+  const { data: resumeLanguages } = useGetAllResumeLanguageQuery(
+    resumeId && !status.includes("addResume") ? { resumeId } : skipToken,
+  );
   ///////////////////////////////////////////// Interests //////////////////////////////////////////////
-  const { data: resumeInterests } = useGetAllResumeInterestsQuery({
-    resumeId: resumeId || "",
-  });
+  const { data: resumeInterests } = useGetAllResumeInterestsQuery(
+    resumeId && !status.includes("addResume") ? { resumeId } : skipToken,
+  );
   ///////////////////////////////////////////// Reference //////////////////////////////////////////////
-  const { data: resumeReference } = useGetAllResumeReferenceQuery({
-    resumeId: resumeId || "",
-  });
+  const { data: resumeReference } = useGetAllResumeReferenceQuery(
+    resumeId && !status.includes("addResume") ? { resumeId } : skipToken,
+  );
 
   useEffect(() => {
     setResumeTemplates(resumeTemplatesData ?? []);
