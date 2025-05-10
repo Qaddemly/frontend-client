9a2e016eed3810c52d536be2cda167d7510ec0dd
Youssef_Gad <youssef.gad2003@yahoo.com>
2025-05-10 17:46:37 +0300

    fix merge conflict

--- src/components/customApplication/ApplicationPersonal.tsx
+++ src/components/customApplication/ApplicationPersonal.tsx
@@ -16,59 +16,6 @@ function ApplicationPersonal() {
   } = useFormContext();
 
   return (
-<<<<<<< applyJob
-    <div className="mx-auto max-w-md rounded-lg bg-white p-6 shadow-md">
-      <div className="mb-3">
-        <InputField errors={errors} id="customfName">
-          <Input
-            register={register}
-            name={"personal.firstName"}
-            options={{ required: "First Name field is required" }}
-            props={{
-              id: "customfName",
-              type: "text",
-              placeholder: "First name",
-            }}
-          />
-        </InputField>
-      </div>
-
-      <div className="mb-3">
-        <InputField errors={errors} id="customlName">
-          <Input
-            register={register}
-            name={"personal.lastName"}
-            options={{ required: "Last Name field is required" }}
-            props={{
-              id: "customlName",
-              type: "text",
-              placeholder: "Last name",
-            }}
-          />
-        </InputField>
-      </div>
-
-      <div className="mb-3">
-        <InputField errors={errors} id="customeMail">
-          <Input
-            register={register}
-            name={"personal.email"}
-            options={{
-              required: "Email is required",
-              pattern: {
-                value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
-                message: "Invalid email address",
-              },
-            }}
-            props={{
-              id: "customeMail",
-              type: "text",
-              placeholder: "Mail",
-            }}
-          />
-        </InputField>
-      </div>
-=======
     <>
       <InputField errors={errors?.personal as FieldErrors} id="firstName">
         <Input
@@ -114,21 +61,15 @@ function ApplicationPersonal() {
           }}
         />
       </InputField>
->>>>>>> dev
 
       <div className="mb-3 flex gap-2">
         <Select
           register={register}
           name="personal.phone.countryCode"
-<<<<<<< applyJob
-          id="customphone"
-          className="w-1/4"
-=======
           id="phone"
           // eslint-disable-next-line @typescript-eslint/ban-ts-comment
           // @ts-expect-error
           className={`${errors?.personal?.phone ? "mb-6" : ""} w-3 sm:w-fit`}
->>>>>>> dev
         >
           {prefixValues.map((value) => (
             <option
@@ -141,11 +82,6 @@ function ApplicationPersonal() {
         </Select>
 
         <InputField
-<<<<<<< applyJob
-          errors={errors}
-          id="customphone"
-          props={{ className: "w-3/4" }}
-=======
           // eslint-disable-next-line @typescript-eslint/ban-ts-comment
           // @ts-expect-error
           errors={errors?.personal?.phone as FieldErrors}
@@ -153,7 +89,6 @@ function ApplicationPersonal() {
           props={{
             className: "w-full",
           }}
->>>>>>> dev
         >
           <Input
             register={register}
@@ -170,15 +105,9 @@ function ApplicationPersonal() {
               },
             }}
             props={{
-<<<<<<< applyJob
-              placeholder: "Phone Number",
-              id: "customphone",
-              type: "text",
-=======
               placeholder: "Phone number",
               id: "number",
               type: "number",
->>>>>>> dev
             }}
           />
         </InputField>
@@ -193,14 +122,6 @@ function ApplicationPersonal() {
           name={"personal.dob"}
           props={{ id: "dob", className: "w-full" }}
         />
-<<<<<<< applyJob
-        {errors?.personal?.dob &&
-          typeof errors.personal.dob.message === "string" && (
-            <span className="text-sm text-danger-300">
-              {errors.personal.dob.message}
-            </span>
-          )}
-=======
         {/*eslint-disable-next-line @typescript-eslint/ban-ts-comment*/}
         {/* @ts-ignore */}
         {errors?.personal?.dob && (
@@ -210,9 +131,8 @@ function ApplicationPersonal() {
             {errors?.personal?.dob?.message as string}
           </span>
         )}
->>>>>>> dev
       </div>
-    </div>
+    </>
   );
 }
 
